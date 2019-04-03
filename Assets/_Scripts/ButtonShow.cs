9195252f6b300c0de40931e493d36b3e745a7e25
John Hay <superjohnhay@gmail.com>
2019-04-03 12:48:45 +0800

    updated 040319

---
+++ Assets/_Scripts/ButtonShow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ButtonShow : MonoBehaviour
+{
+    public GameObject thisButton;
+    public float _countdownTimer;
+
+    private float timer;
+
+
+    public void Start()
+    {
+        thisButton.SetActive(false);
+        timer = _countdownTimer;
+        InvokeRepeating("Countdown", 1.0f, 1.0f);
+    }
+
+
+    void Countdown()
+    {
+        if (--timer == 0.0f)
+        {
+            CancelInvoke("Countdown");
+            thisButton.SetActive(true);
+        }   
+        else
+        {
+            Debug.Log("Countdowning");
+            thisButton.SetActive(false);
+        }
+
+    }
+}
